63c182e91654c9e75de8e24fa4cc7ce6562ba077
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2023-12-28 19:23:17 +0100

    updated project

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+fetch("https://api.adviceslip.com/advice")
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector("span").innerText = data.slip.id;
+    document.querySelector("h1").innerText = `'${data.slip.advice}'`;
+  });
+
 document.querySelector("div").addEventListener("click", () => {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
       document.querySelector("span").innerText = data.slip.id;
-      document.querySelector("h1").innerText = data.slip.advice;
+      document.querySelector("h1").innerText = `'${data.slip.advice}'`;
     })
     .catch((error) => console.error("Error loading API:", error));
 });
